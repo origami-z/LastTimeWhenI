de8100ca089a9b2fc20c8ef0a047b492fa84e12e
Zhihao Cui <davidcui2@me.com>
2020-05-25 16:57:29 +0100

    Initial Commit

---
+++ Last Time When ITests/Last_Time_When_ITests.swift
@@ -0,0 +1,34 @@
+//
+//  Last_Time_When_ITests.swift
+//  Last Time When ITests
+//
+//  Created by Zhihao Cui on 25/05/2020.
+//  Copyright © 2020 zhihaocui. All rights reserved.
+//
+
+import XCTest
+@testable import Last_Time_When_I
+
+class Last_Time_When_ITests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
